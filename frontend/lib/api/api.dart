a2e03dbbfe8d2891cc1508768cee73e18fc5bea8
David Hinterndorfer <david.hinterndorfer@gmail.com>
2025-03-30 00:47:13 +0100

    added api-client

--- frontend/lib/api/api.dart
+++ frontend/lib/api/api.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2025-03-30T00:35:08.505554
+// Openapi Generator last run: : 2025-03-30T00:42:49.656586
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
@@ -6,9 +6,9 @@ import 'package:openapi_generator_annotations/openapi_generator_annotations.dart
         DioProperties(pubName: 'faktura_api', pubAuthor: 'David Hinterndorfer'),
     inputSpec:
         InputSpec(path: '../backend/src/main/openapi/faktura/api_v1.yaml'),
-    // typeMappings: {'Pet': 'ExamplePet'},
     generatorName: Generator.dio,
     runSourceGenOnOutput: true,
-    // outputDirectory: 'api/faktura_api',
-    skipIfSpecIsUnchanged: true)
-class Client {}
+    outputDirectory: 'api/faktura_api',
+    forceAlwaysRun: true,
+    skipIfSpecIsUnchanged: false)
+class Api {}
