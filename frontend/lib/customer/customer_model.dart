9a4a8eec8ccec5f510e5edd1c9b53faa11352b44
David Hinterndorfer <david.hinterndorfer@gmail.com>
2025-04-11 16:39:36 +0200

    paging

--- frontend/lib/customer/customer_model.dart
+++ frontend/lib/customer/customer_model.dart
@@ -1,6 +1,8 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:faktura/app_state_model.dart';
 import 'package:faktura_api/faktura_api.dart';
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class CustomerModel extends ChangeNotifier {
   final AppStateModel _appStateModel;
@@ -8,12 +10,22 @@ class CustomerModel extends ChangeNotifier {
 
   List<Customer> entities = [];
   Customer? selectedEntity;
+  int pageSize = 50;
+  ListBuilder<Sort>? sort = ListBuilder<Sort>([
+    Sort((builder) {
+      builder.property = "name";
+      builder.direction = SortDirectionEnum.ASC;
+    }),
+  ]);
+  PagingState<int, Customer> pagingState = PagingState();
 
   CustomerModel(this._appStateModel, this._customerApi);
 
   void getAll() {
     _appStateModel.setLoading(true);
-    _customerApi.getCustomers().then((response) {
+    _customerApi.getCustomers(pageable: Pageable((builder) {
+      builder.sort = sort;
+    })).then((response) {
       entities = response.data?.content?.toList() ?? [];
       notifyListeners();
       _appStateModel.setLoading(false);
@@ -23,7 +35,36 @@ class CustomerModel extends ChangeNotifier {
     });
   }
 
-  Future<Null> save(Customer customer) {
+  void getNextPage() {
+    if (pagingState.isLoading) {
+      return;
+    }
+
+    pagingState.copyWith(isLoading: true);
+    final nextKey = (pagingState.keys?.last ?? -1) + 1;
+    _customerApi.getCustomers(pageable: Pageable((builder) {
+      builder.pageNumber = nextKey;
+      builder.pageSize = pageSize;
+      builder.sort = sort;
+    })).then((response) {
+      final pageItems = response.data?.content?.toList() ?? [];
+      if (pageItems.isEmpty) {
+        pagingState =
+            pagingState.copyWith(hasNextPage: false, isLoading: false);
+      } else {
+        pagingState = pagingState.copyWith(
+            pages: [...?pagingState.pages, pageItems],
+            keys: [...?pagingState.keys, nextKey],
+            isLoading: false);
+      }
+      notifyListeners();
+    }).catchError((error) {
+      pagingState = pagingState.copyWith(isLoading: false, error: error);
+      notifyListeners();
+    });
+  }
+
+  Future<void> save(Customer customer) {
     _appStateModel.setLoading(true);
     return _customerApi.saveCustomer(customer: customer).then((response) {
       getAll();
--- frontend/lib/customer/customer_screen.dart
+++ frontend/lib/customer/customer_screen.dart
@@ -2,7 +2,9 @@ import 'package:faktura/common/widget/custom_screen.dart';
 import 'package:faktura/customer/customer_form_screen.dart';
 import 'package:faktura/customer/customer_list_item.dart';
 import 'package:faktura/customer/customer_model.dart';
+import 'package:faktura_api/faktura_api.dart';
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:provider/provider.dart';
 
 class CustomerScreen extends StatelessWidget {
@@ -19,13 +21,17 @@ class CustomerScreen extends StatelessWidget {
       form: const CustomerFormScreen(),
       body: Consumer<CustomerModel>(
         builder: (context, model, child) {
-          return ListView.separated(
+          return PagedListView<int, Customer>.separated(
             padding: const EdgeInsets.only(top: 20),
             separatorBuilder: (context, index) => const Divider(),
-            itemBuilder: (context, index) {
-              return CustomerListItem(entry: model.entities[index]);
-            },
-            itemCount: model.entities.length,
+            state: model.pagingState,
+            fetchNextPage:
+            Provider.of<CustomerModel>(context, listen: false).getNextPage,
+            builderDelegate: PagedChildBuilderDelegate(
+              itemBuilder: (context, item, index) {
+                return CustomerListItem(entry: item);
+              },
+            ),
           );
         },
       ),
