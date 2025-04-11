9c2472ddffb62edb66daf163ae75a8cdc6fcd22a
David Hinterndorfer <david.hinterndorfer@gmail.com>
2025-04-11 13:39:33 +0200

    remove searchname; wip paging

---
+++ frontend/api/faktura_api/doc/Sort.md
@@ -0,0 +1,16 @@
+# faktura_api.model.Sort
+
+## Load the model package
+```dart
+import 'package:faktura_api/api.dart';
+```
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**property** | **String** | Property/field to sort by | 
+**direction** | **String** | Sort direction | [default to 'ASC']
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
---
+++ frontend/api/faktura_api/test/sort_test.dart
@@ -0,0 +1,22 @@
+import 'package:test/test.dart';
+import 'package:faktura_api/faktura_api.dart';
+
+// tests for Sort
+void main() {
+  final instance = SortBuilder();
+  // TODO add properties to the builder and call build()
+
+  group(Sort, () {
+    // Property/field to sort by
+    // String property
+    test('to test the property `property`', () async {
+      // TODO
+    });
+
+    // Sort direction
+    // String direction (default value: 'ASC')
+    test('to test the property `direction`', () async {
+      // TODO
+    });
+  });
+}
